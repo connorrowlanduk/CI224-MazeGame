13ae56430eed6508ee2c153fbabaecf04bdd7954
Connor Rowland (student) <cr569@ln-moco-02-10.1.68.12.brighton.ac.uk>
2019-05-29 20:01:21 +0100

    Current Progress

---
+++ Assets/Collect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{ 
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ball get pushed");
+        if (collision.collider.name == "Player")
+        {
+            Debug.Log("delete ball");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+    Text txt;
+    int collectedBall = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        Text txt = gameObject.GetComponent<Text>();
+
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0.0f;
+        float z = 0.0f;
+
+        if (Input.GetKey("w") || Input.GetKey("up"))
+            x += 1.0f;
+        if (Input.GetKey("a") || Input.GetKey("left"))
+            z += 1.0f;
+        if (Input.GetKey("s") || Input.GetKey("down"))
+            x += -1.0f;
+        if (Input.GetKey("d") || Input.GetKey("right"))
+            z += -1.0f;
+
+        transform.Translate(new Vector3(x, 0.0f, z) * 10 * Time.deltaTime, Space.World);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("ball get pushed");
+        if (collision.collider.name == "Collect")
+        {
+            collectedBall++;
+
+            txt.text = collectedBall + " collected ball";
+        }
+    }
+}
